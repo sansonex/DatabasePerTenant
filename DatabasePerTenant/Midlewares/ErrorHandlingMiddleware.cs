625cc7c879e08e2fa86df622c8aca144f0350785
Matheus Sansone Pinheiro <matheussanpinheiro@gmail.com>
2020-11-02 23:08:27 -0300

    Add project files.

---
+++ DatabasePerTenant/Midlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DatabasePerTenant.Midlewares
+{
+    /// <summary>
+    /// Error Handling middleware class.
+    /// </summary>
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="next">The next request delegate.</param>
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        /// <summary>
+        ///     Asynchronous Invoke.
+        /// </summary>
+        /// <param name="context">The HttpContext.</param>
+        /// <returns>The asynchronous operation.</returns>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await this.next(context);
+            }
+            catch (Exception exception)
+            {
+                if (context.Response.HasStarted)
+                {
+                    throw;
+                }
+
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            string responseBody;
+
+            context.Response.Clear();
+
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Response.ContentType = "application/json";
+            responseBody = exception.Message;
+
+            await context.Response.WriteAsync(responseBody);
+        }
+    }
+}
---
+++ DatabasePerTenant/Midlewares/MissingTenantMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabasePerTenant.Midlewares
+{
+    /// <summary>
+    /// Missing Tenant Middleware class.
+    /// </summary>
+    public class MissingTenantMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MissingTenantMiddleware"/> class.
+        /// Missing Tenant constructor.
+        /// </summary>
+        /// <param name="next">Request Delegate.</param>
+        public MissingTenantMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        /// <summary>
+        /// Invoke method.
+        /// </summary>
+        /// <param name="httpContext">http context.</param>
+        /// <param name="provider">provider.</param>
+        /// <returns>nothing at all.</returns>
+        public async Task Invoke(HttpContext httpContext, ITenantProvider provider)
+        {
+            if (await provider.GetTenantAsync() == null)
+            {
+                throw new TenantNotFoundException();
+            }
+
+            await this.next.Invoke(httpContext);
+        }
+    }
+}
